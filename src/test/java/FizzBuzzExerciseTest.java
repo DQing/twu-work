702dd55d028bcdadd51ea542ce329ed8e88ccfd2
Qingqing Dou <qqdou@thoughtworks.com>
2018-09-02 16:31:44 +0800

    add more test and ignore build
    Signed-off-by: Qingqing Dou <qqdou@thoughtworks.com>

--- src/test/java/FizzBuzzExerciseTest.java
+++ src/test/java/FizzBuzzExerciseTest.java
@@ -4,10 +4,19 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class FizzBuzzExerciseTest {
     @Test
-    void should_print_correct_fizz_buzz() {
+    void should_print_correct_fizz_buzz_when_input_15() {
         String[] except = new String[]{"1", "2", "Fizz", "4", "Buzz", "Fizz", "7", "8",
                 "Fizz", "Buzz", "11", "Fizz", "13", "14", "FizzBuzz"};
         int number = 15;
+        String[] actual = FizzBuzzExercise.getFizzBuzz(number);
+        assertArrayEquals(except, actual);
+    }
+
+    @Test
+    void should_print_correct_fizz_buzz_when_input_10() {
+        String[] except = new String[]{"1", "2", "Fizz", "4", "Buzz", "Fizz", "7", "8",
+                "Fizz", "Buzz"};
+        int number = 10;
         String[] actual = FizzBuzzExercise.getFizzBuzz(number);
         assertArrayEquals(except, actual);
     }
--- src/test/java/PrimeFactorsExerciseTest.java
+++ src/test/java/PrimeFactorsExerciseTest.java
@@ -8,9 +8,17 @@ import static org.junit.jupiter.api.Assertions.assertIterableEquals;
 
 class PrimeFactorsExerciseTest {
     @Test
-    void should_print_prime_factor() {
+    void should_print_prime_factor_when_length_20() {
         List<Integer> except = Arrays.asList(1, 2, 3, 5, 7, 11, 13, 17, 19);
         int input = 20;
+        ArrayList<Integer> actual = PrimeFactorsExercise.getPrimeFactor(input);
+        assertIterableEquals(except, actual);
+    }
+
+    @Test
+    void should_print_prime_factor_when_length_30() {
+        List<Integer> except = Arrays.asList(1, 2, 3, 5, 7, 11, 13, 17, 19, 23, 29);
+        int input = 30;
         ArrayList<Integer> actual = PrimeFactorsExercise.getPrimeFactor(input);
         assertIterableEquals(except, actual);
     }
